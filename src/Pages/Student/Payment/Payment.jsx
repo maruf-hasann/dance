072f8536c384bb4b8a700351c8c3622946250849
Maruf Hasan <114363016+maruf-hasann@users.noreply.github.com>
2023-06-14 04:32:45 +0600

    Update Payment.jsx

--- src/Pages/Student/Payment/Payment.jsx
+++ src/Pages/Student/Payment/Payment.jsx
@@ -1,28 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Checkout from './Checkout';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 import { useParams } from 'react-router-dom';
-
+import axios from 'axios';
+import { useQuery } from 'react-query';
+import { data } from 'autoprefixer';
 const stripePromise = loadStripe(
   "pk_test_51NI5FcLRwCFm07SK4zdCZImA9BifMajbMwmMi2grZBqv0XE7HrNlx5XPCowk4427mNNDxOBWLou7qMByLLviJAVH00lLVk8ahL"
 );
-// console.log(stripePromise);
-
 const Payment = () => {
-    const  amount  = useParams()
-    
-    const price = parseFloat(amount.price)
-    
-    return (
-      <div>
-        <h2>Payment</h2>
-        <Elements stripe={stripePromise}>
-          <Checkout price={price}></Checkout>
-        </Elements>
-        
-      </div>
-    );
+  const { price} = useParams()
+ 
+
+
+  const classPrice = Number(price);
+  console.log(classPrice);
+  return (
+    <div>
+      <h1>payment</h1>
+      <Elements stripe={stripePromise}>
+        <Checkout price={classPrice}></Checkout>
+      </Elements>
+    </div>
+  );
 };
 
 export default Payment;
