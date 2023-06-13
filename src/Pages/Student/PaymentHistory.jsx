5f5eec25fbd4b972255a66e4ccf22c21bf27461c
Maruf Hasan <114363016+maruf-hasann@users.noreply.github.com>
2023-06-14 04:38:06 +0600

    Update PaymentHistory.jsx

--- src/Pages/Student/PaymentHistory.jsx
+++ src/Pages/Student/PaymentHistory.jsx
@@ -1,1 +1,49 @@
 
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import useAuth from "../../../Hooks/useAuth";
+
+const PaymentHistory = () => {
+  const { user } = useAuth();
+  const [payment, setPayment] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://dance-ten.vercel.app/payment-history?email=${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => setPayment(data));
+  }, []);
+
+  const history = payment.reverse()
+  return (
+    <div className="overflow-x-auto">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th></th>
+            <th>Email</th>
+            <th>Transaction ID</th>
+
+            <th>Amount</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {history.map((pay,index) => (
+            <tr key={pay._id}>
+              <td>{index + 1}</td>
+              <td>{pay?.email}</td>
+              <td>{pay?.transactionId}</td>
+              <td>{pay?.price}</td>
+              <td>{pay?.date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default PaymentHistory;
+
+
